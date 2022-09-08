11c370a9f43993cdfd3b7f1b67ff0d28f44c8dee
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2022-09-08 16:26:34 -0300

    motoremaza.gemspec and motoremaza_spec.rb

--- spec/motoremaza_spec.rb
+++ spec/motoremaza_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Motoremaza do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Motoremaza::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end

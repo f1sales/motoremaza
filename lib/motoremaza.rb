46b0c5df6357753203f42e0bd95abf7152cf26e4
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2022-09-08 16:04:13 -0300

    Git init

---
+++ lib/motoremaza.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "motoremaza/version"
+
+module Motoremaza
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/motoremaza_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Motoremaza do
+  it "has a version number" do
+    expect(Motoremaza::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

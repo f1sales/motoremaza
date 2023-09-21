531f4d136634e44452d00fa15bb5f0c7d9554836
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-09-21 17:59:10 -0300

    Email is back

--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -187,8 +187,8 @@ RSpec.describe F1SalesCustom::Hooks::Lead do
       end
 
       it 'returns nil source' do
-        expect(switch_source).to be_nil
-        # expect(switch_source).to eq('Email da Honda')
+        # expect(switch_source).to be_nil
+        expect(switch_source).to eq('Email da Honda')
       end
     end
 
